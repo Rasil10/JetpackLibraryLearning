16ce2951a2d6ae92ebdc6d6b80239b9949439540
Rasil <Rasil>
2022-04-07 12:31:32 +0545

    navigation learning 1

--- app/src/main/java/com/rasil/viewmodel1/MainActivity.kt
+++ app/src/main/java/com/rasil/viewmodel1/MainActivity.kt
@@ -8,20 +8,10 @@ import com.rasil.viewmodel1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewModel: MainActivityViewModel
-    private lateinit var viewModelFactory: MainActivityViewModelFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        viewModelFactory = MainActivityViewModelFactory(100)
-        viewModel = ViewModelProvider(this, viewModelFactory).get(MainActivityViewModel::class.java)
 
-        with(binding) {
-            viewModel = viewModel
-            lifecycleOwner = this@MainActivity
-
-
-        }
     }
 }
