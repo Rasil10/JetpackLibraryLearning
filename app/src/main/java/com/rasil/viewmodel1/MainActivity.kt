1b23f36d43da71593a3a13c63aa9815b975e047c
Rasil <rasilr10@gmail.com>
2022-05-18 15:06:24 -0500

    coroutine

--- app/src/main/java/com/rasil/viewmodel1/MainActivity.kt
+++ app/src/main/java/com/rasil/viewmodel1/MainActivity.kt
@@ -3,15 +3,38 @@ package com.rasil.viewmodel1
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
 import com.rasil.viewmodel1.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        binding.btnCount.setOnClickListener {
+            binding.countTV.text = count++.toString()
+        }
+
+        binding.downloadUserDataButton.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                downloadUserData()
+            }
+        }
+    }
+
+    private suspend fun downloadUserData() {
+        for (i in 1..200000) {
+
+            withContext(Dispatchers.Main) {
+//            Log.i( "download","Downloading User $i in ${Thread.currentThread().name}")
+                binding.userMessageTV.text = "Downloading User $i in ${Thread.currentThread().name}"
+            }
+        }
     }
 }
